c217b9b7e959d1669fd561c7a5e6e1447f356887
Ali Najm <alikang867@gmail.com>
2026-04-19 01:56:39 +0300

    Fix 404: Re-added dist folder with multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
+
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: true
-  },
-  base: './',
   build: {
-    outDir: 'dist'
-  }
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu.html'),
+      },
+    },
+  },
 })
